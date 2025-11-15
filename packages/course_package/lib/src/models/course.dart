be65793e365828021f1f8ff3c1d898e9152a3802
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-11-15 19:25:21 +0800

    refactor: rename to course item for course list

--- packages/course_package/lib/src/models/course.dart
+++ packages/course_package/lib/src/models/course.dart
@@ -1,4 +1,6 @@
+import 'package:course_package/src/enums/course_category.dart';
 import 'package:course_package/src/enums/course_type.dart';
+import 'package:course_package/src/enums/difficulty_level.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'course.freezed.dart';
@@ -8,17 +10,22 @@ part 'course.g.dart';
 abstract class Course with _$Course {
   const Course._();
 
-  factory Course({
+  factory Course.item({
     required String id,
     required String title,
     required String description,
     required DateTime dateAdded,
+    required DateTime startDate,
     required double ceus,
     required double price,
+    required List<String> targetAudiences,
+    required CourseTopic topic,
+    required DifficultyLevel difficultyLevel,
     @Default(false) bool isLive,
     @Default(false) bool isPrerecordedWebinar,
     @Default(false) bool isGamifiedCourse,
-  }) = _Course;
+    @Default(false) bool isFreeForMembers,
+  }) = CourseItem;
 
   factory Course.fromJson(Map<String, dynamic> json) => _$CourseFromJson(json);
 
