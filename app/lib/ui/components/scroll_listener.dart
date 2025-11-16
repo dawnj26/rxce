dfe896976f07a67c91405de8db6189f4c7513b0c
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-11-16 21:35:08 +0800

    chore(ui): add scroll listener

---
+++ app/lib/ui/components/scroll_listener.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class ScrollListener extends StatefulWidget {
+  const ScrollListener({
+    required this.slivers,
+    super.key,
+    this.onLoadMore,
+    this.threshold = 0.9,
+    this.physics,
+  });
+
+  final void Function()? onLoadMore;
+  final List<Widget> slivers;
+  final double threshold;
+  final ScrollPhysics? physics;
+
+  @override
+  State<ScrollListener> createState() => _ScrollListenerState();
+}
+
+class _ScrollListenerState extends State<ScrollListener> {
+  final _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.onLoadMore != null) {
+      _scrollController.addListener(_onScroll);
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      controller: _scrollController,
+      slivers: widget.slivers,
+      physics: widget.physics,
+    );
+  }
+
+  bool get _isBottom {
+    if (!_scrollController.hasClients) return false;
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    return currentScroll >= (maxScroll * widget.threshold);
+  }
+
+  void _onScroll() {
+    if (_isBottom) {
+      widget.onLoadMore?.call();
+    }
+  }
+}
