1899a449f0f50cc797d5ac0057fc81a4b241e84f
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-11-16 21:33:08 +0800

    chore: add course list route

--- app/lib/router/router.dart
+++ app/lib/router/router.dart
@@ -1,9 +1,12 @@
+import 'package:app/ui/course/view/list/course_list_page.dart';
 import 'package:auto_route/auto_route.dart';
+
+part 'router.gr.dart';
 
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
-    /// routes go here
+    AutoRoute(page: CourseListRoute.page, initial: true),
   ];
 }
