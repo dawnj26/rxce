994e88776fcefbfc4aa55fbe5f8ad64f8be807c5
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-12-11 11:22:36 +0800

    chore: add api package

---
+++ packages/api_package/lib/api_package.dart
@@ -0,0 +1,5 @@
+/// API Client Package
+library;
+
+export 'src/api_package.dart';
+export 'src/models/models.dart';
---
+++ packages/api_package/lib/src/api_package.dart
@@ -0,0 +1,7 @@
+/// {@template api_package}
+/// API Client Package
+/// {@endtemplate}
+class ApiPackage {
+  /// {@macro api_package}
+  const ApiPackage();
+}
