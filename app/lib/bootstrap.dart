79c1c8ef42e7f39c0a8dd7cf33dee4b601bdefd3
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-12-15 21:19:00 +0800

    chore: initialize api and logger

--- app/lib/bootstrap.dart
+++ app/lib/bootstrap.dart
@@ -1,16 +1,29 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:api_package/api_package.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
+import 'package:talker/talker.dart';
 import 'package:talker_bloc_logger/talker_bloc_logger_observer.dart';
+import 'package:talker_dio_logger/talker_dio_logger.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  Bloc.observer = TalkerBlocObserver();
+  final talker = Talker();
+  Bloc.observer = TalkerBlocObserver(talker: talker);
+
+  ApiPackage.initialize(
+    ApiConfig(
+      baseUrl: 'https://rxce.com/',
+      interceptors: [
+        TalkerDioLogger(talker: talker),
+      ],
+    ),
+  );
 
   runApp(await builder());
 }
