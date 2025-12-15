fe3558665f1f0145f745d1362d1e05c2ac6669dc
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-12-15 21:22:10 +0800

    chore: add ce bloc listener

--- app/lib/ui/course/view/list/course_list_page.dart
+++ app/lib/ui/course/view/list/course_list_page.dart
@@ -2,7 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:course_package/course_package.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rxce/bloc/course/course_list_bloc.dart';
+import 'package:rxce/bloc/ce/ce_bloc.dart';
+import 'package:rxce/bloc/course/list/course_list_bloc.dart';
 import 'package:rxce/ui/course/view/list/course_list_screen.dart';
 
 @RoutePage()
@@ -11,7 +12,17 @@ class CourseListPage extends StatelessWidget implements AutoRouteWrapper {
 
   @override
   Widget build(BuildContext context) {
-    return const CourseListScreen();
+    return BlocListener<CeBloc, CeState>(
+      listenWhen: (p, c) => p.ceRequirement != c.ceRequirement,
+      listener: _handleListener,
+      child: const CourseListScreen(),
+    );
+  }
+
+  void _handleListener(BuildContext context, CeState state) {
+    context.read<CourseListBloc>().add(
+      CourseListEvent.ceRequirementChanged(state.ceRequirement),
+    );
   }
 
   @override
