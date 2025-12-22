0a523e1bf2693fb0cbfb03d1250bd678de57e688
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-12-22 15:25:49 +0800

    fix: exports

--- app/lib/ui/course/course.dart
+++ app/lib/ui/course/course.dart
@@ -1,4 +1,4 @@
 export 'view/course_page.dart';
 export 'view/list/course_list_page.dart';
 export 'view/search/course_search_page.dart';
-export 'view/search/result/course_search_result_page.dart';
+export 'view/search_result/course_search_result_page.dart';
