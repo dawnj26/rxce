433eb407b9e897c7c15067c47a63cb71d089e6ce
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-01-06 16:43:43 +0800

    chore: add theme package for app

---
+++ packages/theme_package/lib/src/theme_package.dart
@@ -0,0 +1,7 @@
+/// {@template theme_package}
+/// A Flutter package providing theming capabilities for the RXCE application.
+/// {@endtemplate}
+class ThemePackage {
+  /// {@macro theme_package}
+  const ThemePackage();
+}
