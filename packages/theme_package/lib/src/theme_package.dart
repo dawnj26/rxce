c093f28fc0205b8cd2563c7d6750c54a87ce1cde
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-01-10 17:24:31 +0800

    chore: add light theme

--- packages/theme_package/lib/src/theme_package.dart
+++ packages/theme_package/lib/src/theme_package.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:theme_package/src/themes/themes.dart';
+
 /// {@template theme_package}
 /// A Flutter package providing theming capabilities for the RXCE application.
 /// {@endtemplate}
 class ThemePackage {
-  /// {@macro theme_package}
-  const ThemePackage();
+  const ThemePackage._();
+
+  /// Default light theme data
+  static ThemeData get light => ThemeData.light().copyWith(
+    extensions: [
+      AppColorTheme.light,
+      AppTextTheme.light,
+    ],
+  );
 }
