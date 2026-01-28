00f554e2c6c5e695df3131536c592646924135cd
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-01-29 00:56:52 +0800

    chore: add authentication routes

--- app/lib/router/router.dart
+++ app/lib/router/router.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/widgets.dart';
+import 'package:rxce/ui/auth/auth.dart';
 import 'package:rxce/ui/course/course.dart';
 
 part 'router.gr.dart';
@@ -8,6 +9,8 @@ part 'router.gr.dart';
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
+    AutoRoute(page: SignInRoute.page, path: '/signin', initial: true),
+    AutoRoute(page: RegistrationRoute.page, path: '/signup'),
     AutoRoute(
       page: CourseRoute.page,
       initial: true,
