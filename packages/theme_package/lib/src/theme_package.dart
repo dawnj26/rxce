f875359115924c27904c11a32cf6912b1172beff
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-01-29 22:37:09 +0800

    refactor: use ButtonStyle to customize button states

--- packages/theme_package/lib/src/theme_package.dart
+++ packages/theme_package/lib/src/theme_package.dart
@@ -14,16 +14,34 @@ class ThemePackage {
       seedColor: AppColorTheme.light.primary.shade500,
     ),
     filledButtonTheme: FilledButtonThemeData(
-      style: FilledButton.styleFrom(
-        textStyle: AppTextTheme.light.labelLarge.copyWith(
-          fontWeight: FontWeight.w700,
+      style: ButtonStyle(
+        textStyle: WidgetStatePropertyAll(
+          AppTextTheme.light.labelLarge.copyWith(
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        overlayColor: const WidgetStatePropertyAll(
+          Colors.transparent,
         ),
         splashFactory: NoSplash.splashFactory,
-        backgroundColor: AppColorTheme.light.primary.shade500,
-        foregroundColor: AppColorTheme.light.primary.shade50,
-        padding: const EdgeInsets.symmetric(
-          vertical: 16,
-          horizontal: 24,
+        backgroundColor: WidgetStateProperty.resolveWith<Color?>((states) {
+          if (states.contains(WidgetState.pressed)) {
+            return AppColorTheme.light.primary.shade600;
+          }
+          if (states.contains(WidgetState.disabled)) {
+            return AppColorTheme.light.neutral.shade200;
+          }
+          return AppColorTheme.light.primary.shade500;
+        }),
+        foregroundColor: WidgetStateProperty.fromMap({
+          WidgetState.disabled: AppColorTheme.light.neutral.shade400,
+          WidgetState.any: AppColorTheme.light.primary.shade50,
+        }),
+        padding: const WidgetStatePropertyAll(
+          EdgeInsets.symmetric(
+            vertical: 16,
+            horizontal: 24,
+          ),
         ),
       ),
     ),
