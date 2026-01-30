4032b6e7d95706cbb14b0b18c2adad7c0d70eea6
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-01-30 16:43:24 +0800

    feat(ui): add errorText and enabled props to password field

--- app/lib/shared/components/password_field.dart
+++ app/lib/shared/components/password_field.dart
@@ -3,10 +3,18 @@ import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:rxce/l10n/l10n.dart';
 
 class PasswordField extends StatefulWidget {
-  const PasswordField({super.key, this.onChanged, this.textInputAction});
+  const PasswordField({
+    super.key,
+    this.onChanged,
+    this.textInputAction,
+    this.errorText,
+    this.enabled,
+  });
 
   final void Function(String value)? onChanged;
   final TextInputAction? textInputAction;
+  final String? errorText;
+  final bool? enabled;
 
   @override
   State<PasswordField> createState() => _PasswordFieldState();
@@ -18,9 +26,11 @@ class _PasswordFieldState extends State<PasswordField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      enabled: widget.enabled,
       obscureText: _obscureText,
       onChanged: widget.onChanged,
       decoration: InputDecoration(
+        errorText: widget.errorText,
         labelText: context.l10n.passwordHint,
         suffixIcon: IconButton(
           onPressed: () {
