ea341adad638e05ca52b7645db98096b7cf45ad9
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-01-31 22:32:26 +0800

    feat(ui): add label widget with * for required fields

--- app/lib/shared/components/components.dart
+++ app/lib/shared/components/components.dart
@@ -1,6 +1,7 @@
 export 'app_logo.dart';
 export 'filter_button.dart';
 export 'form_wrapper.dart';
+export 'label.dart';
 export 'password_field.dart';
 export 'scroll_listener.dart';
 export 'select_card.dart';
