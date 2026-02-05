103f08f8cac96055bfb9f94ec7e2ba43cbb021dc
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-02-05 21:36:43 +0800

    feat(router): add dashboard nav routes

--- app/lib/router/router.dart
+++ app/lib/router/router.dart
@@ -1,7 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/widgets.dart';
+import 'package:rxce/ui/account/account.dart';
 import 'package:rxce/ui/auth/auth.dart';
+import 'package:rxce/ui/browse/browse.dart';
+import 'package:rxce/ui/cart/cart.dart';
 import 'package:rxce/ui/course/course.dart';
+import 'package:rxce/ui/dashboard/dashboard.dart';
+import 'package:rxce/ui/home/home.dart';
+import 'package:rxce/ui/learn/learn.dart';
 
 part 'router.gr.dart';
 
@@ -21,6 +27,18 @@ class AppRouter extends RootStackRouter {
         AutoRoute(page: CourseListRoute.page, initial: true, path: ''),
         AutoRoute(page: CourseSearchRoute.page, path: 'search'),
         AutoRoute(page: CourseSearchResultRoute.page, path: 'search/:query'),
+      ],
+    ),
+    AutoRoute(
+      page: DashboardRoute.page,
+      path: '/',
+      initial: true,
+      children: [
+        AutoRoute(page: HomeRoute.page, path: 'home', initial: true),
+        AutoRoute(page: LearnRoute.page, path: 'learn'),
+        AutoRoute(page: BrowseRoute.page, path: 'browse'),
+        AutoRoute(page: CartRoute.page, path: 'cart'),
+        AutoRoute(page: AccountRoute.page, path: 'account'),
       ],
     ),
   ];
