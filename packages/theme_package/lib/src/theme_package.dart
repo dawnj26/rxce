549ea6af3301dfd5672240fbfeef7924901c3bcb
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-02-12 00:24:57 +0800

    refactor: convert to function initializing the app text

--- packages/theme_package/lib/src/theme_package.dart
+++ packages/theme_package/lib/src/theme_package.dart
@@ -9,59 +9,72 @@ class ThemePackage {
   const ThemePackage._();
 
   /// Default light theme data
-  static ThemeData get light => ThemeData.light().copyWith(
-    scaffoldBackgroundColor: Colors.white,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: AppColorTheme.light.primary.shade500,
-    ),
-    filledButtonTheme: FilledButtonThemeData(
-      style: ButtonStyle(
-        textStyle: WidgetStatePropertyAll(
-          AppTextTheme.light.labelLarge.copyWith(
-            fontWeight: FontWeight.w700,
+  static ThemeData light() {
+    final textTheme = AppTextTheme.light();
+
+    return ThemeData.light().copyWith(
+      appBarTheme: AppBarTheme(
+        backgroundColor: AppColorTheme.light.neutral.shade50,
+        foregroundColor: AppColorTheme.light.neutral.shade900,
+        elevation: 0,
+        titleTextStyle: textTheme.heading5.copyWith(
+          fontWeight: FontWeight.w700,
+          color: AppColorTheme.light.neutral.shade900,
+        ),
+      ),
+      scaffoldBackgroundColor: Colors.white,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: AppColorTheme.light.primary.shade500,
+      ),
+      filledButtonTheme: FilledButtonThemeData(
+        style: ButtonStyle(
+          textStyle: WidgetStatePropertyAll(
+            textTheme.labelLarge.copyWith(
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          overlayColor: WidgetStatePropertyAll(
+            AppColorTheme.light.primary.shade700,
+          ),
+          backgroundColor: WidgetStateProperty.fromMap({
+            WidgetState.disabled: AppColorTheme.light.neutral.shade200,
+            WidgetState.any: AppColorTheme.light.primary.shade500,
+          }),
+          foregroundColor: WidgetStateProperty.fromMap({
+            WidgetState.disabled: AppColorTheme.light.neutral.shade400,
+            WidgetState.any: AppColorTheme.light.primary.shade50,
+          }),
+          padding: const WidgetStatePropertyAll(
+            EdgeInsets.symmetric(
+              vertical: 16,
+              horizontal: 24,
+            ),
           ),
         ),
-        overlayColor: WidgetStatePropertyAll(
-          AppColorTheme.light.primary.shade700,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: AppColorTheme.light.primary.shade500,
+            width: 2,
+          ),
         ),
-        backgroundColor: WidgetStateProperty.fromMap({
-          WidgetState.disabled: AppColorTheme.light.neutral.shade200,
-          WidgetState.any: AppColorTheme.light.primary.shade500,
-        }),
-        foregroundColor: WidgetStateProperty.fromMap({
-          WidgetState.disabled: AppColorTheme.light.neutral.shade400,
-          WidgetState.any: AppColorTheme.light.primary.shade50,
-        }),
-        padding: const WidgetStatePropertyAll(
-          EdgeInsets.symmetric(
-            vertical: 16,
-            horizontal: 24,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: AppColorTheme.light.neutral.shade300,
           ),
         ),
       ),
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      floatingLabelBehavior: FloatingLabelBehavior.never,
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8),
-        borderSide: BorderSide(
-          color: AppColorTheme.light.primary.shade500,
-          width: 2,
-        ),
-      ),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8),
-        borderSide: BorderSide(
-          color: AppColorTheme.light.neutral.shade300,
-        ),
-      ),
-    ),
-    textTheme: GoogleFonts.montserratTextTheme(),
-    extensions: [
-      AppColorTheme.light,
-      AppTextTheme.light,
-    ],
-  );
+      textTheme: GoogleFonts.montserratTextTheme(),
+      extensions: [
+        AppColorTheme.light,
+        textTheme,
+      ],
+    );
+  }
 
   /// Default padding value
   static const double padding = 16;
