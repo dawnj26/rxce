6cd8a053e0648cf053d86db7ca0c51dfd61ab30f
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-02-12 00:37:29 +0800

    refactor: separate ceu badge

--- app/lib/ui/course/components/course_item_card.dart
+++ app/lib/ui/course/components/course_item_card.dart
@@ -51,32 +51,6 @@ class CourseItemCard extends StatelessWidget {
               ),
             ),
           ),
-        );
-      },
-    );
-  }
-}
-
-class _Ceu extends StatelessWidget {
-  const _Ceu({
-    required this.ceus,
-  });
-
-  final double ceus;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
-      decoration: BoxDecoration(
-        color: context.appColor.primary.shade500,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Text(
-        '$ceus CEU',
-        style: context.appText.labelMedium.copyWith(
-          color: context.appColor.primary.shade50,
-          fontWeight: FontWeight.w600,
         ),
       ),
     );
